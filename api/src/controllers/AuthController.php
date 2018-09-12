924536768d6aac384669719a2ac4dac178389376
Mlax Wong <mlaxwong@gmail.com>
2018-09-12 21:40:57 +0800

    working on rest api

--- api/src/controllers/AuthController.php
+++ api/src/controllers/AuthController.php
@@ -6,6 +6,14 @@ use Paws;
 
 class AuthController extends Controller
 {
+    // public function behaviors()
+    // {
+    //     $behaviors = parent::behaviors();
+    //     $behaviors['verbs'] = ['class' => VerbFilter::className(), 'actions' => ['login' => ['POST', 'OPTIONS']]];
+    //     $behaviors['authenticator'] = ['class' => CompositeAuth::className(), 'except' => ['login'], 'authMethods' => [QueryParamAuth::className()]];
+    //     return $behaviors;
+    // }
+
     public function actionIndex()
     {
         return 'test';
