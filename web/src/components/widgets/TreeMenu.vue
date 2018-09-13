4db375bfc825376344d7ac006414931e7120f59d
Mlax Wong <mlaxwong@gmail.com>
2018-09-13 20:49:28 +0800

    working on tree menu

--- web/src/components/widgets/TreeMenu.vue
+++ web/src/components/widgets/TreeMenu.vue
@@ -1,20 +1,24 @@
 <template>
-    <ul>
-        {{ label }}
-        <TreeMenu v-for="(item, index) in this.items" :key="index" :items="item.items" :label="item.label">
-        </TreeMenu>
+    <ul v-if="!init">
+        <TreeMenu v-for="(item, index) in this.items" :key="index" :items="item.items" :item="item" :label="item.label" :init="true" />
     </ul>
+    <li v-else>
+        {{ label }}
+        <ul v-if="item.items">
+            <TreeMenu v-for="(item, index) in this.items" :key="index" :items="item.items" :item="item" :label="item.label" :init="true" />
+        </ul>
+    </li>
 </template>
 
 <script>
 export default {
     name: 'TreeMenu',
-    props: ['items', 'label'],
-    // data() {
-    //     return {
-    //         items: this.items
-    //     };
-    // },
+    props: [
+        'items', 
+        'label', 
+        'item',
+        'init'
+    ],
     components: {'TreeMenu': this}
 }
 </script>
